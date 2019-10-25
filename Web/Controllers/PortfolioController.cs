b0ff1184048abbd1da9397cc8a240f7ab8712189
MD. SOJIB MIYA <md.sojibmiya@gmail.com>
2019-10-25 12:31:29 +0600

    Added Default Route

---
+++ Web/Controllers/PortfolioController.cs
@@ -0,0 +1,22 @@
+﻿using ApplicationCore.Entities.Portfolio;
+using ApplicationCore.Interfaces.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    public class PortfolioController : ApiControllerBase
+    {
+        private readonly IEfRepository<Projects> _projectRepository;
+
+        public PortfolioController(IEfRepository<Projects> projectRepository)
+        {
+            _projectRepository = projectRepository;
+        }
+
+        public IActionResult Get()
+        {
+            var response = new string[] { "Hello", "World" };
+            return Ok(response);
+        }
+    }
+}
